5e39da9492ee618541a77ce6a3caf8290d58a6d5
Toufiq-it <ittoufikit@gmail.com>
2022-04-18 22:31:18 +0600

    8th commit

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useParams } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const ServiceDetails = () => {
-    const { serviceId } = useParams();
+    const [user] = useAuthState(auth);
+    const [email, setEmail ] = useState('');
+    const { serviceId  } = useParams();
     return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
+
+        <div className='container responsive-width w-50 mx-auto'>
+            <h2 className='text-primary text-center fw-bold mt-2'>Process Information</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control type="name" placeholder="Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control value={user?.email} readOnly type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicAddress">
+                    <Form.Control type="text" placeholder="Address" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPHone">
+                    <Form.Control type="phone" placeholder="Phone" required />
+                </Form.Group>
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                Proceed Checkout
+                </Button>
+            </Form>
+            
         </div>
     );
 };
