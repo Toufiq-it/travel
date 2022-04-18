3a3cbf289c442c88587a3bdcc44f70d3d72f7506
Toufiq-it <ittoufikit@gmail.com>
2022-04-18 13:44:40 +0600

    3rd commit

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -10,7 +10,7 @@ const Service = ({service}) => {
         navigate(`/service/${id}`);
     }
     return (
-        <div className='g-5 col-sm-12 col-md-6 col-lg-4'>
+        <div className='g-5 col-sm-12 col-md-6 col-lg-4 mt-5'>
             <div className="card" style={{ width: "18rem" }}>
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../../logo/travel.png'
 
 const Header = () => {
     return (
-        <header>
-            <h2>this is Header</h2>
-        </header>
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} height="40" alt="" />
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            
+        </>
     );
 };
 
